7575c4549a4ca1b696321108c2f8c59f657762ca
Michael Gwynne <michael@votemike.co.uk>
2019-04-25 22:52:30 +0100

    Updates

--- precache-manifest.5d183717b93b91f2056ad45a4e5a509f.js
+++ precache-manifest.5d183717b93b91f2056ad45a4e5a509f.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2d7923eae3e7efc0752010f096de1c61",
+    "revision": "830b67014717e04feb07f9fa8c13fe11",
     "url": "/todo/index.html"
   },
   {
-    "revision": "c42b250d44d9be51a49d",
-    "url": "/todo/static/css/main.bc4de4d2.chunk.css"
+    "revision": "c264f83ee220d4afd281",
+    "url": "/todo/static/css/main.b632f0ca.chunk.css"
   },
   {
     "revision": "5c9ff16ec5bee154aee2",
     "url": "/todo/static/js/2.5286435d.chunk.js"
   },
   {
-    "revision": "c42b250d44d9be51a49d",
-    "url": "/todo/static/js/main.14ff7300.chunk.js"
+    "revision": "c264f83ee220d4afd281",
+    "url": "/todo/static/js/main.f6ceb561.chunk.js"
   },
   {
     "revision": "3ce2d46cb5510c372f4d",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/todo/precache-manifest.6aba3cbfc92de80f50cb05ef2f6c9074.js"
+  "/todo/precache-manifest.5d183717b93b91f2056ad45a4e5a509f.js"
 );
 
 self.addEventListener('message', (event) => {
