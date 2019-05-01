15c78fe79363b4235390ea444a9c650c8741594e
Michael Gwynne <michael@votemike.co.uk>
2019-05-01 16:52:39 +0100

    Starting Chart

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.scss';
 import TodoList from './components/TodoList';
 import AddTodo from './components/AddTodo';
+import { Line as LineChart } from 'react-chartjs';
 
 function App() {
   const useStateWithLocalStorage = storageKey => {
@@ -76,6 +77,31 @@ function App() {
     );
   }
 
+  const chartData = {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [{
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2, 3],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)'
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)'
+      ],
+      borderWidth: 1
+    }]
+  };
+  const chartOptions = {};
 
   return (
       <div>
@@ -86,6 +112,9 @@ function App() {
           <AddTodo submitHandler={addTodoHandler} />
           {sortButton}
           {todoList}
+        </div>
+        <div className="chart">
+          <LineChart data={chartData} options={chartOptions} width="600" height="250"/>
         </div>
         <footer>
           <a href="https://github.com/votemike/todo#Tah-Do">About Tah-Do</a>
