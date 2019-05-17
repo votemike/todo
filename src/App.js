08d8d1177af99e09c961279efb97b83f8cb7cd31
Michael Gwynne <michael@votemike.co.uk>
2019-05-17 18:42:09 +0100

    WiP

--- src/App.js
+++ src/App.js
@@ -5,22 +5,51 @@ import TodoList from './components/TodoList';
 import AddTodo from './components/AddTodo';
 
 function App() {
-  const useStateWithLocalStorage = storageKey => {
-    const [todos, setTodos] = React.useState(
-        JSON.parse(localStorage.getItem(storageKey)) || [
-      { effort: 1, impact: 10, text: "Add to Home screen" },
-      { effort: 2, impact: 9, text: "Add some todos" },
-      { effort: 3, impact: 4, text: "Test offline" }
-    ]);
+  let search = window.location.search;
+  let params = new URLSearchParams(search);
+  let foo = params.get('jql');
+  console.log(encodeURI(foo));
+  let headers = new Headers();
+  fetch(`${baseJiraUrl}/issues/?jql=status%20!%3D%20Closed%20AND%20project%20%3D%20Michael%20and%20assignee%20is%20EMPTY%20and%20component%20%3D%20%22Discover%20team%22%20and%20%22Epic%20Link%22%20%3D%20Michael-68`, {headers: headers})
+      .then(res => res.json())
+      .then((out) => {
+        console.log('Checkout this JSON! ', out);
+      })
+      .catch(err => { throw err });
+
+  const useStateWithLocalStorage = (storageKey, defaultTodos = null) => {
+    const [value, setValue] = React.useState(JSON.parse(localStorage.getItem(storageKey)) || defaultTodos);
 
     React.useEffect(() => {
-      localStorage.setItem(storageKey, JSON.stringify(todos));
-    }, [todos, storageKey]);
+      localStorage.setItem(storageKey, JSON.stringify(value));
+    }, [value, storageKey]);
 
-    return [todos, setTodos];
+    return [value, setValue];
   };
 
-  const [todos, setTodos] = useStateWithLocalStorage('todos');
+  const defaultTodos = [
+    { effort: 1, impact: 10, text: "Add to Home screen" },
+    { effort: 2, impact: 9, text: "Add some todos" },
+    { effort: 3, impact: 4, text: "Test offline" }
+  ];
+  const [todos, setTodos] = useStateWithLocalStorage('todos', defaultTodos);
+  const [importing, setImporting] = useStateWithLocalStorage('importing');
+
+  React.useEffect((foo, setTodos, todos) => {
+    if (foo && todos.length > 0) {
+      if (window.confirm("Would you like to replace this list with your JIRA results?")) {
+        setTodos([]);
+        fetch(`${baseJiraUrl}/rest/api/2/search?${foo}`)
+            .then(res => res.json())
+            .then((out) => {
+              console.log('Checkout this JSON! ', out);
+            })
+            .catch(err => { throw err });
+
+      }
+      // window.location.href = '/';
+    }
+  }, []);
 
   const addTodoHandler = event => {
     event.preventDefault();
