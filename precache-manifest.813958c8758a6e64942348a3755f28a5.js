006a8e8c38f1a48ea09147c93fdeb758b1eab71e
Michael Gwynne <michael@votemike.co.uk>
2019-05-23 14:26:45 +0100

    Updates

--- precache-manifest.813958c8758a6e64942348a3755f28a5.js
+++ precache-manifest.813958c8758a6e64942348a3755f28a5.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e9d3c72bd24a016db1b52485deb4d395",
+    "revision": "4815caf02963cbc68685540043aab3eb",
     "url": "/todo/index.html"
   },
   {
-    "revision": "2c96666a123805206d33",
-    "url": "/todo/static/css/main.25fdb9b9.chunk.css"
+    "revision": "84dbbaaded29cc7ccf59",
+    "url": "/todo/static/css/main.a28fcd75.chunk.css"
   },
   {
     "revision": "0c81a32e29e42a4fd150",
     "url": "/todo/static/js/2.ba90e030.chunk.js"
   },
   {
-    "revision": "2c96666a123805206d33",
+    "revision": "84dbbaaded29cc7ccf59",
     "url": "/todo/static/js/main.06783fb7.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/todo/precache-manifest.5620b4dfb9314bebe19a1901bb8e71b7.js"
+  "/todo/precache-manifest.813958c8758a6e64942348a3755f28a5.js"
 );
 
 self.addEventListener('message', (event) => {
