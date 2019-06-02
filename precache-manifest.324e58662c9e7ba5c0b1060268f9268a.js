94cb9237217f8582c11b962d0118bb5d80497c06
Michael Gwynne <michael@votemike.co.uk>
2019-06-02 10:59:17 +0100

    Updates

--- precache-manifest.324e58662c9e7ba5c0b1060268f9268a.js
+++ precache-manifest.324e58662c9e7ba5c0b1060268f9268a.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d82cfecc61d338dd4a7840c3e70b50ca",
+    "revision": "aa6be1c5267a6fe807c9e88cdba4e24c",
     "url": "/todo/index.html"
   },
   {
-    "revision": "c2f84ec42953481b2e8b",
-    "url": "/todo/static/css/main.ac545289.chunk.css"
+    "revision": "542224f55bac3888e0b9",
+    "url": "/todo/static/css/main.a460e32c.chunk.css"
   },
   {
     "revision": "0c81a32e29e42a4fd150",
     "url": "/todo/static/js/2.ba90e030.chunk.js"
   },
   {
-    "revision": "c2f84ec42953481b2e8b",
-    "url": "/todo/static/js/main.7b072579.chunk.js"
+    "revision": "542224f55bac3888e0b9",
+    "url": "/todo/static/js/main.7ba835a6.chunk.js"
   },
   {
     "revision": "3ce2d46cb5510c372f4d",
