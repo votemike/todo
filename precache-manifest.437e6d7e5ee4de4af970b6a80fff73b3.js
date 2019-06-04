33c6d738c9a4f8f7a4b608bfa75d3a9bd5522c20
Michael Gwynne <michael@votemike.co.uk>
2019-06-04 16:15:10 +0100

    Updates

---
+++ precache-manifest.437e6d7e5ee4de4af970b6a80fff73b3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d5828eb9c2a82d099726f4d9b206b7b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a6cfccb15ddee5f73b29",
+    "url": "./static/css/main.3bffc114.chunk.css"
+  },
+  {
+    "revision": "0c81a32e29e42a4fd150",
+    "url": "./static/js/2.ba90e030.chunk.js"
+  },
+  {
+    "revision": "a6cfccb15ddee5f73b29",
+    "url": "./static/js/main.c460b35d.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
